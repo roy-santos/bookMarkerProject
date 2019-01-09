080f713f5c0ee9c1d4a430de918f13571ca585a8
Roy <roysantos112@gmail.com>
2019-01-09 09:07:05 -0800

    JS line 52 error still there

--- main.js
+++ main.js
@@ -1,5 +1,62 @@
 document.getElementById('myForm').addEventListener("submit", saveBookmark);
 
-function saveBookmark() {
-    console.log('It works');
+//Save bookmark
+function saveBookmark(e) {
+    //Get form values
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+    
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+
+    /*Local Storage Test
+    localStorage.setItem('test', 'Hello World');
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+    console.log(localStorage.getItem('test'));
+    */
+
+    //Test if bookmarks is null
+    if(localStorage.getItem('bookmarks') === null) {
+        //Init array
+        var bookmarks = [ ];
+        //Add to array
+        bookmarks.push(bookmark);
+        //Set to LocalStorage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        //Get bookmarks from LocalStorage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        //Add bookmarks to array
+        bookmarks.push(bookmark);
+        //Re-set back to LocalStorage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }  
+ 
+    //Prevent form from submitting
+    e.preventDefault();
+}
+
+//Fetch bookmarks
+function fetchBookmarks() {
+    //Get bookmarks from LocalStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
+
+    //Get outbput id
+    var bookmarksResults = document.getElementById('bookmarksResults');
+
+    //Build output
+    bookmarksResults.innerHTML = '';
+    for(var i=0; i<bookmarks.length; i++){
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += '<div class="well">'+
+                                      '<h3>'+name+
+                                      '<a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> '
+                                      '</h3>'
+                                      '</div>';
+    }
 }
